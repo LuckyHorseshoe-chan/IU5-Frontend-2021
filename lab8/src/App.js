3dd956bf865dcc605678e5785b08f38206053c7a
Lucky Horseshoe <lucky@ya.ru>
2021-04-19 22:29:58 +0300

    Another way to sort checkboxes

--- lab8/src/App.js
+++ lab8/src/App.js
@@ -22,27 +22,25 @@ function Button({ title, children, onClick, className}){
   );
 }
 
-// function Swap(lst, i1, i2){
-//   let temp = lst[i1];
-//   lst[i1] = lst[i2];
-//   lst[i2] = temp;
-// }
+function Swap(lst, i1, i2){
+  let temp = lst[i1];
+  lst[i1] = lst[i2];
+  lst[i2] = temp;
+}
 
 function App() {
   const [notes, setNotes] = React.useState([{ select: false, title: 'task1'}, {select: false, title: 'task2'}, 
   {select: false, title: 'task3'}]);
- // const [count, setCount] = React.useState(notes.length - 1);
+ const [count, setCount] = React.useState(notes.length - 1);
  // const [select, setSelect] = React.useState(false);
  // const NoteList = [<div>Hello, world!</div>, <div>Buy, world!</div>]
   const handleClick = () => {
-    //let ind; 
-    //alert(count)
-  //   newValues.length > 1 ? setCount(count + 1) : setCount(0);
-  //   //alert(count)
-  //   newValues.length > 1 ? ind = newValues.length - 1 : ind = 0;
-  //  // alert(ind)
-  //   Swap(newValues, ind, count)
-    setNotes([...notes, {select: false, title:'taskN'}]);
+    let ind; 
+    const newValues = [...notes, {select: false, title:'taskN'}];
+    // newValues.length > 1 ? setCount(count + 1) : setCount(0);
+    // newValues.length > 1 ? ind = newValues.length - 1 : ind = 0;
+    // Swap(newValues, ind, count)
+    setNotes(newValues);
     //setNotes([...notes, {select: false, title:'taskN'}]);
     //alert(notes)
   }
@@ -51,10 +49,10 @@ function App() {
     const sel = event.target.checked;
     const newValues = [...notes];
     newValues[index].select = sel;
-    setNotes(newValues);
+    //setNotes(newValues);
     // Swap(newValues, index, count);
     // count > 0 ? setCount(count - 1) : setCount(0);
-    // setNotes(newValues);
+    setNotes(newValues);
     
   }
   //const [ShowNote, setShowNote] = React.useState('1');
@@ -63,7 +61,7 @@ function App() {
   const removeNote = (index) => {
     console.log(index)
     setNotes(notes.filter((_, index1) => index1 != index));
-    //count > 0 ? setCount(count - 1) : setCount(0);
+    // count > 0 ? setCount(count - 1) : setCount(0);
     //alert(notes)
   }
   const changeInput = (index) => (event) => {
@@ -72,10 +70,23 @@ function App() {
     newValues[index].title = val;
     setNotes(newValues);
   }
+  console.log(notes.sort(({select: select1}, {select: select2}) => select1 > select2 ? 1 : -1));
   return (
     <div className="App">
       <h2>ToDo:</h2>
-        {notes.map((item, index) => 
+      
+      {notes.sort(({select: select1}, {select: select2}) => select1 < select2).map((item, index) => 
+          <label key={index}>
+            <AddNote 
+            select={item.select} 
+            onChangeCheckbox={handleChangeCheckbox(index)} 
+            title={item.title} 
+            onChange={changeInput(index)}
+            />
+            <Button className="delButton" onClick={() => removeNote(index)}/>
+          </label>)}
+          <Button className="addButton" title='Add a task' onClick={handleClick}/>
+        {/* {notes.map((item, index) => 
         !item.select ?
           <label key={index}>
             <AddNote 
@@ -97,7 +108,7 @@ function App() {
             />
             <Button className="delButton" onClick={() => removeNote(index)}/>
           </label> : '')}
-        <Button className="addButton" title='Add a task' onClick={handleClick}/>
+        <Button className="addButton" title='Add a task' onClick={handleClick}/> */}
     </div>
   );
   // const IfShowNote = () => {
